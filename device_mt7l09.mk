37a2226aa855d6e8789e241d9ff3111a481f4e36
gabry3795 <gabry.gabry@hotmail.it>
2015-10-19 21:46:12 +0200

    Added some info in device_[codename].mk
    Change-Id: Ie49e6c4ff27e2d6240bf9d041f74108c1117f5f3

--- device_mt7l09.mk
+++ device_mt7l09.mk
@@ -1,3 +1,19 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
@@ -24,6 +40,17 @@ PRODUCT_NAME := full_mt7l09
 PRODUCT_DEVICE := mt7l09
 
 # ---------------------------------------------------------------------------
+
+# This device is xxhdpi.  However the platform doesn't
+# currently contain all of the bitmaps at xxhdpi density so
+# we do this little trick to fall back to the hdpi version
+# if the xhdpi doesn't exist.
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+DEVICE_RESOLUTION := 1080x1920
 
 LOCAL_PATH := device/huawei/mt7l09
 
