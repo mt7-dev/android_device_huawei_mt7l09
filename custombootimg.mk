92c9117bb61ca3026c552e88b18972016afc445b
gabry3795 <gabry.gabry@hotmail.it>
2016-04-15 23:07:05 +0200

    Removed sepolicy original files and reorganized the root for ramdisk

--- custombootimg.mk
+++ custombootimg.mk
@@ -9,6 +9,6 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(INSTALLED_RAMDISK_TARGET)
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 $(INSTALLED_RECOVERYIMAGE_TARGET): $(recovery_ramdisk) $(recovery_kernel) $(HOST_OUT_EXECUTABLES)/mkbootimg-mt7
-	@echo ----- Making recovery image ------
+	@echo ----- Making CUSTOM recovery image ------
 	$(hide) $(HOST_OUT_EXECUTABLES)/mkbootimg-mt7 --kernel $(recovery_kernel) --ramdisk $(recovery_ramdisk) $(BOARD_MKBOOTIMG_ARGS) --output $(INSTALLED_RECOVERYIMAGE_TARGET)
-	@echo ----- Made recovery image -------- $@
+	@echo ----- Made CUSTOM recovery image -------- $@
